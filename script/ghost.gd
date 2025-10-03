358f5e224ce4ca991ed0a726799c86655348a57e
SSSYLSSS <120182557+SSSYLSSS@users.noreply.github.com>
2025-10-03 21:26:39 +0800

    time_machine

--- script/ghost.gd
+++ script/ghost.gd
@@ -1,6 +1,7 @@
 extends Sprite2D
 
 func _physics_process(delta: float) -> void:
-	modulate.a = lerpf(modulate.a, 0, 0.1)
+	modulate.a = lerpf(modulate.a, 0, 0.2)
+	modulate.r = lerpf(modulate.r, 0, 0.1)
 	if modulate.a < 0.05:
 		queue_free()
